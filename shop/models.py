7bb467c51f7f6d0ab64abb47754a2aab81019342
coosoti <charlesosoti@gmail.com>
2018-05-09 10:28:30 +0300

    add templates to display list products and product details

--- shop/models.py
+++ shop/models.py
@@ -1,8 +1,7 @@
 """Models for products and Category"""
 from django.db import models
 
-from django.db import models
-
+from django.urls import reverse
 
 class Category(models.Model):
     """This will hold all categories with products in it"""
@@ -16,6 +15,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])    
 
 class Product(models.Model):
     """This model holds product details"""
@@ -34,3 +36,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])    
