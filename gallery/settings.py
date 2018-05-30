1c6ba5cab6c7e693e9b3a10d8534aadeeb4aec54
coosoti <charlesosoti@gmail.com>
2018-05-30 15:57:24 +0300

    change static root settings

--- gallery/settings.py
+++ gallery/settings.py
@@ -132,7 +132,7 @@ CART_SESSION_ID = 'cart'
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
