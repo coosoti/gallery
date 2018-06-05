8dba8d79daafab9a02e2221ae175d7c7aec9fbd9
coosoti <charlesosoti@gmail.com>
2018-06-05 15:44:21 +0300

    Fix decimal_places bug

--- shop/models.py
+++ shop/models.py
@@ -26,7 +26,7 @@ class Product(models.Model):
     slug = models.SlugField(max_length=200, db_index=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=10, decimal_places=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
